265418ae0331016194ea693e5295904eecc1f3ec
abolfazl mashhadi <abolfazlmashhadi93@gmail.com>
2024-12-10 14:48:57 +0330

    added jwt module added and configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/coffee_bar')],
+  imports: [
+    ConfigModule.forRoot({}),
+    MongooseModule.forRoot(process.env.DB_URL),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_PASS,
+      signOptions: {
+        expiresIn: '1w',
+      },
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
