8552072144a080bc114e7a1c3ca1aa1317cdef3c
abolfazl mashhadi <abolfazlmashhadi93@gmail.com>
2024-12-26 14:50:00 +0330

    updated main file for cors support

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+  
+  });
   app.enableVersioning({
     type: VersioningType.URI,
     prefix: 'api/v',
